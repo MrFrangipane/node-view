5479728baa81775d356c1c2927159a26ce5174db
colin <colin-one@hotmail.com>
2016-11-15 21:28:07 +0100

    finish doc and add a loop breaker

--- python/slots.py
+++ python/slots.py
@@ -5,8 +5,13 @@ DEFAULT_OUTPUT_COLOR = (.95, .05, .95)
 
 
 class AbstractSlot(object):
-
     def __init__(self, name, color=DEFAULT_INPUT_COLOR, parent_node=None):
+        """
+        Base class for slot object, slot hold methode for connect and disconnect node
+        :param name: slot's name, also visible in node ui
+        :param color: slot's color
+        :param parent_node: parent node
+        """
         # Init Members
         self.parent_node = parent_node
         self.name = name.capitalize()
@@ -15,10 +20,11 @@ class AbstractSlot(object):
         self.connected_slots = list()
         self.connected_edges = list()
         self.connect_data = ''
-
+        #path name of the connect to slot
         self.connect_to = ''
+        #path name of this slot
         self.path_name = ''
-
+        #implementation SlotPySide
         self.implementation = None
 
     def connect(self, slot):
@@ -27,12 +33,26 @@ class AbstractSlot(object):
     def disconnect(self, slot):
         raise NotImplementedError
 
-
 class InputSlot(AbstractSlot):
-
     def connect(self, output_slot):
+        """
+        connect the output_slot to this input slot
+        :param output_slot: output slot
+        :return: None
+        """
         # Assert Type
         assert isinstance(output_slot, OutputSlot), "Connect Input to output only !"
+        #loop breaker
+        def sss(node):
+            for slot in node.input_slots:
+                for output_slot in slot.connected_slots:
+                    if output_slot.parent_node == self.parent_node:
+                        return True
+                    return sss(output_slot.parent_node)
+        loop = sss(output_slot.parent_node)
+        if loop:
+            print 'looped'
+            return
         # If already connected
         if output_slot in self.connected_slots:
             return
@@ -50,6 +70,11 @@ class InputSlot(AbstractSlot):
         ))
 
     def disconnect(self, output_slot):
+        """
+        Disconnect the output slot to this slot
+        :param output_slot:
+        :return:
+        """
         # If present
         if output_slot in self.connected_slots:
             # Update Members
@@ -65,12 +90,26 @@ class InputSlot(AbstractSlot):
             target_slot=self.name
         ))
 
-
 class OutputSlot(AbstractSlot):
-
     def connect(self, input_slot):
+        """
+        connect the input_slot to this output slot
+        :param output_slot: output slot
+        :return: None
+        """
         # Assert Type
         assert isinstance(input_slot, InputSlot), "Connect Output to input only !"
+        #loop breaker
+        def sss(node):
+            for slot in node.output_slots:
+                for output_slot in slot.connected_slots:
+                    if output_slot.parent_node == self.parent_node:
+                        return True
+                    return sss(output_slot.parent_node)
+        loop = sss(input_slot.parent_node)
+        if loop:
+            print 'looped'
+            return
         # If already connected
         if input_slot in self.connected_slots:
             return
@@ -88,6 +127,11 @@ class OutputSlot(AbstractSlot):
         ))
 
     def disconnect(self, input_slot):
+        """
+        Disconnect the input slot to this slot
+        :param output_slot:
+        :return:
+        """
         # If present
         if input_slot in self.connected_slots:
             # Update Members
